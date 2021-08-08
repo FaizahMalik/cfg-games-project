657b40fd0f5c2500d00b3557911181493f2e253c
HalynaMaslak <halyna.maslak@gmail.com>
2021-08-08 23:04:20 +0100

    new create_json file, fixed word_picker, new words.json

---
+++ create_json_wordsfile.py
@@ -0,0 +1,39 @@
+from nltk import FreqDist
+from nltk.corpus import brown, stopwords
+from nltk.stem import WordNetLemmatizer
+
+import json
+
+# The Brown corpus (published in 1961) consists of 1M words from American English distributed across 15 genres.
+br_words = brown.tagged_words(tagset='universal')
+
+# Lemmatize means to use the dictionary form of the word, e.g. cats - cat, knew - know.
+lemmatizer = WordNetLemmatizer()
+
+# Stop words are very common words that do not add much to the meaning (articles, pronouns, prepositions, auxiliary verbs).
+# We don't want the users to guess such words in our game so we need to filter them from the Brown corpus.
+stop_words = stopwords.words('english')
+
+# Part-of-speech (POS) tags in the Brown corpus (tagset='universal')
+tags = {"ADJ", "ADV", "NOUN", "VERB"}
+# POS tags in WordNetLemmatizer are different.
+wn_tags = {"ADJ": "a", "ADV": "r", "NOUN": "n", "VERB": "v"}
+
+filtered_words = [(lemmatizer.lemmatize(word.lower(), pos=wn_tags[tag]), tag)  # a list of tuples: lemmatized lowercase word and its POS tag
+                  for word, tag in br_words
+                  if word.isalpha()  # exclude punctuation
+                  and word.lower() not in stop_words
+                  and tag in tags  # choose only adjectives, adverbs, nouns, and verbs from the Brown corpus
+                  ]
+
+# FreqDist (frequency distribution) is used to find the frequency of words within a text. It returns a dictionary of words and their count.
+fd = FreqDist(filtered_words)
+
+freq_words = fd.most_common(5000) # a list of 5000 word, tag tuples and their count sorted in the descending order.
+# [(('would', 'VERB'), 2714), (('make', 'VERB'), 2312)...]
+
+final_wordlist = [tup for tup, count in freq_words if len(tup[0]) > 3 ] # a list of tuples: word, tag. Since it is already sorted, we do not need the count anymore.
+
+with open("words_list.json", "w") as file:
+  res = json.dumps(final_wordlist)
+  file.write(res)
--- word_picker.py
+++ word_picker.py
@@ -1,30 +1,15 @@
-from nltk import FreqDist
-from nltk.corpus import brown, stopwords
-from nltk.stem import WordNetLemmatizer
-
 from collections import Counter
 import random
+import json
 
 class WordPicker:
 
     def __init__(self):
-        """Creates a list of tuples (word, POS tag) of 5000 most frequent words in the Brown corpus
-        and a list of only words."""
-        br_words = brown.tagged_words(tagset='universal')
-        lemmatizer = WordNetLemmatizer()
-        stop_words = stopwords.words('english')
-        tags = {"ADJ", "ADV", "NOUN", "VERB"}
-        wn_tags = {"ADJ": "a", "ADV": "r", "NOUN": "n", "VERB": "v"}
-        filtered_words = [(lemmatizer.lemmatize(word.lower(), pos=wn_tags[tag]), tag)
-                          for word, tag in br_words
-                          if word.isalpha()
-                          and word.lower() not in stop_words
-                          and tag in tags
-                          ]
-        fd = FreqDist(filtered_words)
-        freq_words = fd.most_common(5000)
-        final_wordlist = [tup for tup, count in freq_words if len(tup[0]) > 3 ]
-        self.all_words = final_wordlist
+        """Reads from the words_list.json file (5000 most common words from the Brown corpus)
+         and creates a list of lists [word, POS tag] and a list of only words."""
+        with open("words_list.json", "r") as json_file:
+            final_words_list = json.load(json_file)
+        self.all_words = final_words_list
         self.only_words = [word for word, tag in self.all_words]
 
     def get_freq_pos(self, pos):
@@ -40,7 +25,7 @@ class WordPicker:
             return False
 
     def is_palindrome(self, word):
-        """Returns True is the word is a palindrome."""
+        """Returns True is the word is a palindrome. Otherwise, returns False."""
         return word == word[::-1]
 
     def get_word_of_task(self, task_num):
@@ -52,47 +37,50 @@ class WordPicker:
 
         elif task_num == 2:
             message = "Try to guess a common adjective in American English."
-            common_adj = [w for w in self.get_freq_pos("ADJ")[:80] if len(w) > 6]
+            common_adj = [w for w in self.get_freq_pos("ADJ")[:100] if 10 > len(w) > 6]
             word_to_guess = random.choice(common_adj)
 
         elif task_num == 3:
-            message = "Let's make it more difficult. This time, the word might be a noun, adjective, adverb or verb."
-            any_word = [w for w in self.only_words[:100] if len(w) > 6]
+            message = "Let's make it more difficult. This time, the word might be a noun, adverb or verb."
+            any_word = [w for w, tag in self.all_words[:300] if 10 > len(w) > 7 and tag != "ADJ" and not self.is_unique_chars(w)]
             word_to_guess = random.choice(any_word)
 
         elif task_num == 4:
-            message = "Can you guess a word where all the letters are different?"
-            unique = [w for w in self.only_words if self.is_unique_chars(w) and len(w) > 5]
-            word_to_guess = random.choice(unique)
-
-        elif task_num == 5:
             message = "Palindromes are words that read the same backward or forward. Can you guess one?"
-            palindromes = [w for w in self.only_words if self.is_palindrome(w) and len(w) > 1]
+            palindromes = [w for w in set(self.only_words) if self.is_palindrome(w) and 10 > len(w) > 1]
             word_to_guess = random.choice(palindromes)
 
+        elif task_num == 5:
+            message = "Can you guess a word where all the letters are different?"
+            unique = [w for w in self.only_words if self.is_unique_chars(w) and len(w) == 7]
+            word_to_guess = random.choice(unique)
+
         elif task_num == 6:
-            message = "Try to guess a noun that consists of 10 unique characters."
-            unique_nouns = [w for w in self.get_freq_pos("NOUN") if len(w) == 10 and self.is_unique_chars(w)]
+            message = "Try to guess a noun that consists of 9 unique characters."
+            unique_nouns = [w for w in self.get_freq_pos("NOUN") if len(w) == 9 and self.is_unique_chars(w)]
             word_to_guess = random.choice(unique_nouns)
 
         elif task_num == 7:
             message = "How about an adjective? Think twice, it is not the most common one."
-            long_adj = [w for w in self.get_freq_pos("ADJ")[-40:] if len(w) > 8]
+            long_adj = [w for w in self.get_freq_pos("ADJ")[-110:] if len(w) == 9]
             word_to_guess = random.choice(long_adj)
 
         elif task_num == 8:
             message = "A less frequent noun, verb or adverb. What will you get this time?"
-            rare_words = [w for w, tag in self.all_words[-50:] if len(w) > 7 and tag != "ADJ"]
-            word_to_guess = random.choice(rare_words)
+            rare_words = [w for w, tag in self.all_words if 10 > len(w) > 7 and tag != "ADJ"]
+            word_to_guess = random.choice(rare_words[-50:])
 
         elif task_num == 9:
             message = "How well do you know adverbs? Can you guess a long one?"
-            long_adv = [w for w in self.get_freq_pos("ADV") if len(w) > 10]
+            long_adv = [w for w in self.get_freq_pos("ADV") if len(w) == 10]
             word_to_guess = random.choice(long_adv)
 
         elif task_num == 10:
-            message = "You've worked hard but there's one more task left. It's time for actions, so guess a verb."
-            long_verb = [w for w in self.get_freq_pos("VERB") if len(w) > 10]
+            message = "You've worked hard but there's one more task left. It's time for action, so guess a verb."
+            long_verb = [w for w in self.get_freq_pos("VERB") if len(w) == 10]
             word_to_guess = random.choice((long_verb))
 
         return (message, word_to_guess)
+
+
+
