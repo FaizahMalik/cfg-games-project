aa6221cc37eb04f9b1197f35de925f5b6afac6d0
vanessasjoborg <sjoborgprivat@outlook.com>
2021-08-14 12:42:33 +0100

    - Put script into two functions - welcome_message() & play_hangman to avoid repeating code if player wants to play again - added potential code to allow user the option to play again

--- main.py
+++ main.py
@@ -1,42 +1,74 @@
-from levels import Beginner  #, Medium, Hard
+from levels import Beginner, Medium, Hard
 import time
 
-# TODO put in function to avoid repetition of code
+
 # DONE rename main.py to main.py
+# DONE put script into two functions - welcome_message() & play_hangman to avoid repeating code if player
+# wants to play again
+# DONE Added the option to play again
+# TODO reset turtle so drawing starts over again
 
-defaultList = ['python']  #, 'software', 'list', 'dictionary', 'string', 'tuple', 'programming', 'function', 'class']
 
-string = "\n" + " WELCOME TO WORDGUESSER ".center(44, "=") + "\n\n"
-for char in string:
-    print(char, end='')
-    time.sleep(.05)
+def welcome_message():
+    string = "\n" + " WELCOME TO WORDGUESSER ".center(44, "=") + "\n\n"
+    for char in string:
+        print(char, end='')
+        time.sleep(.05)
 
-if input("Do you want to play wordguesser? y/n: ").lower().strip() == "y":
-    username = input("Enter your name: ")
-else:
-    print("Maybe next time!")
-    exit()
+    if input("Do you want to play wordguesser? y/n: ").lower().strip() == "y":
+        username = input("Enter your name: ")
+        play_hangman(Beginner, username)
+        return username
+    else:
+        print("Maybe next time!")
+        exit()
 
-if input("Hi, {}. Would you like to use your own custom words? y/n: ".format(username)) == "y":
-    customList = input("Please enter the words separated by a comma, e.g. car, plane, ... \n").lower().split(", ")
-    game1 = Beginner(username, customList)
-else:
-    print(f"You will be using the default list. Your word is hidden below...\n")
-    game1 = Beginner(username, defaultList)
 
-game1.pick_word()
-print(game1.show_word())
+def play_hangman(level, username):
 
-while game1.display_word.replace(' ', '') != game1.chosen_word:
-    guess = input(f"\nAttempts left: {game1.attempts}\nPast Guesses: {game1.past_guesses}\n\nEnter your guess: ")
-    game1.incorrect_guess(guess)
-    if game1.attempts <= 0:
-        break
+    defaultList = ['python']  #, 'software', 'list', 'dictionary', 'string', 'tuple', 'programming', 'function', 'class']
 
-string = "\n" + " THANKS FOR PLAYING! ".center(44, "=") + "\n\n"
-for char in string:
-    print(char, end='')
-    time.sleep(.05)
+    if input("Hi, {}. Would you like to use your own custom words? y/n: ".format(username)) == "y":
+        customList = input("Please enter the words separated by a comma, e.g. car, plane, ... \n").lower().split(", ")
+        game1 = level(username, customList)
+    else:
+        print(f"You will be using the default list. Your word is hidden below...\n")
+        game1 = level(username, defaultList)
+
+    game1.pick_word()
+    print(game1.show_word())
+
+    while game1.display_word.replace(' ', '') != game1.chosen_word:
+        guess = input(f"\nAttempts left: {game1.attempts}\nPast Guesses: {game1.past_guesses}\n\nEnter your guess: ")
+        game1.incorrect_guess(guess)
+        if game1.attempts <= 0:
+            break
+
+    string = "\n" + " THANKS FOR PLAYING! ".center(44, "=") + "\n\n"
+    for char in string:
+        print(char, end='')
+        time.sleep(.05)
+
+    ## POTENTIAL CODE TO ALLOW USER TO PLAY AGAIN??####
+    # if input("Do you want to play again? y/n: ").lower().strip() == "y":
+    #     user_level = input("Which level would you like to play? Beginner/Medium/Hard/Wildcard: ").capitalize().strip()
+    #     available_levels = {
+    #         'Beginner': Beginner,
+    #         'Medium': Medium,
+    #         'Hard': Hard
+    #         # 'Wildcard': Wildcard
+    #     }
+    #
+    #     if user_level in available_levels:
+    #         level = available_levels[user_level]
+    #     else:
+    #         print("Level does not exist.")
+    #     play_hangman(level, username)
+    # else:
+    #     print("Maybe next time!")
+    #     exit()
+
+welcome_message()
 
 
 ### ALTERNATIVE MESSAGES:
